f9c2371e363c3d84507a1b6401a00330bd393f3b
DanielSilva <daniel.sobral.silva.9@gmail.com>
2014-03-19 17:23:18 +0000

    Changed the current version of xstrstr method.

--- Serie 1/xstrstr.c
+++ Serie 1/xstrstr.c
@@ -3,7 +3,7 @@
 char *xstrstr(const char * haystack , const char * needle);
 
 int main(){
-	const char message[] = "escolafd";
+	const char message[] = "escxxcolafd";
 	const char key[] = "cola";
 	char * ret;
 	ret = xstrstr( message , key );
@@ -11,26 +11,29 @@ int main(){
 	return 0;
 }
 
-char * xstrstr(const char * haystack , const char * needle){
+char * xstrstr(const char *haystack , const char * needle){
+	// verifica se os ponteiros estão vazios
 	if(needle == '\0') // se needle tiver vazia retorna o ponteiro para o inicio da 1a string
 		return  (char*)haystack; // haystack = ponteiro , *haystack = conteudo do endereço	
-	const char* haystack_copy = NULL ;	
-	const char* needle_copy = needle;
-	while( *haystack != '\0' ){ //enquanto nao se chegar ao fim da 1a string
-		if( *needle_copy == '\0' )
-			break;
-		else{
-			if( *needle_copy == *haystack ){ // se o conteudo de memoria for igual nos 2 (mesma letra)
-				needle_copy++;
-				if( haystack_copy == NULL)
-					haystack_copy = haystack;
-			}
-			else if ( needle_copy != needle ) {
-				haystack_copy = NULL; // solucao ma
-				needle_copy = needle;
-			}		
+
+	//Inicializa variáveis
+	const char *haystack_copy, *needle_copy, *haystack_current;
+	haystack_copy = haystack_current = haystack;
+	needle_copy = needle;	
+
+	//Procura a palavra
+	while( *haystack_copy != '\0' ){ //enquanto nao se chegar ao fim da 1a string
+		while((*needle_copy != '\0')&&(*needle_copy == *haystack_copy)){ // se o conteudo de memoria for igual nos 2 (mesma letra)
+			needle_copy++;
+			haystack_copy++;
 		}
-		++haystack;
+		if(*needle_copy == '\0')
+			return (char *)haystack_current;
+		else{
+			needle_copy = needle;
+			haystack_copy = ++haystack_current;
+		}			
 	}
-	return (char*)haystack_copy;
+	
+	return  NULL;
 }
