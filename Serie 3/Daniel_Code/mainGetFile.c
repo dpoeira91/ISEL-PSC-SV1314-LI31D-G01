e40133b115b4a0924faebc1373de7fad7e3a7135
DanielSilva <daniel.sobral.silva.9@gmail.com>
2014-05-31 18:34:52 +0100

    added code to test

--- Serie 3/Daniel_Code/mainGetFile.c
+++ Serie 3/Daniel_Code/mainGetFile.c
@@ -1,24 +1,13 @@
 #include <stdio.h>
-#include <curl/curl.h>
-#include <dlfcn.h>
-
-int http_get_file(const char * url, const char * filename);
+#include "libWork.h"
 
 int main(){
-	char * url;
-
-	void * h = dlopen( "./mylib.so" , RTLD_NOW );
-	*(void**)&http_get_file = dlsym(h, "http_get_file");
-	
+	char * url;	
 	url = "http://www.isel.pt/docs/img/fundo2.jpg";
-	
 	char * filename;
-	
 	filename = "test.out";
 	int a;	
 	a = http_get_file(url, filename);
-	dlclose((void*)h);
-	
 	return a;
 
 }
--- Serie 3/Daniel_Code/mainJson.c
+++ Serie 3/Daniel_Code/mainJson.c
@@ -3,23 +3,14 @@
 #include <string.h>
 #include <jansson.h>
 #include <curl/curl.h>
-#include <dlfcn.h>
-//#include "mylib.h"
-
-void * (* http_get_json)(const char * url, int * retcod);
+#include "workLib.h"
 
 int main(){
-	void * h = dlopen( "./mylib.so" , RTLD_NOW );
-	*(void**)&http_get_json = dlsym(h, "http_get_json");
-	
-	
 	json_t * final;	
 	char * url;
 	url = "http://headers.jsontest.com/";
 	int *retcod;
 	retcod = 0;
-	final = http_get_json(url, retcod);
-	dlclose((void*)h);
-	
+	final = (void*) http_get_json(url, retcod);
 	return 0;
 }
